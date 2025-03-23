3e8be04d2513b6d2001197074c66d63a6c2aba63
John Brust <johnbrust2002@gmail.com>
2025-03-22 21:09:54 -0500

    Feature: Add connection to a local mysql server.
    Used mysql2 node package to connect to a local sql server and serve a query when get request is processed. Also added dotenv for mananging of the database passwords.

--- back-end/app.js
+++ back-end/app.js
@@ -1,18 +1,54 @@
-const express = require("express");
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors'
+import mysql from 'mysql2/promise';
+import "dotenv/config.js";
 
+
+//Connect to database, replace with your own .env file
+var test_database = await mysql.createConnection({
+    host: "localhost",    //Set name of network host
+    user: "root",         //Set username, in this case root
+    password: process.env.DATABASE_PASS, //Set password
+    database: "test_schema"
+}) 
+
+
+//Start Database
+test_database.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+
+var result = "default value"; 
+
+//Query the database
+try{
+    const [results] = await test_database.query(
+        'SELECT * FROM `test_table`'
+    );
+
+    result = results;
+
+} catch (err) {
+    console.log(err);
+}
+
+
+//Now start the server
 const app = express();
-
 app.use(cors())
 
+
+//If you send an http post request. 
 app.post("/", (req, res) => {
     console.log("Connected to React");
     res.redirect("localhost:3000");
 });
 
-
+//If you send an http get request.
 app.get("/", (req, res) => {
-    res.send("Connected to Express");
+    res.send(result);
 })
 
 const PORT = process.env.PORT || 8080;
