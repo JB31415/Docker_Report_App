0627d4a3659559ae89e65e4317a7522762a52bc0
John Brust <johnbrust2002@gmail.com>
2025-04-02 11:02:10 -0500

    Bug: Fix bug relating to returning the promise of queryDatabase() instead of the values. Also re-added some minor code that was deleted in commit #f5acca3

--- back-end/app.js
+++ back-end/app.js
@@ -1,16 +1,21 @@
+//Node Server
 import express from 'express';
+//Allows for cross site API requests
 import cors from 'cors'
+//Add basic SQL features for MySQL
 import mysql from 'mysql2/promise';
+//Dotenv add environment variables, not in use currently
 import "dotenv/config.js";
+//Parses POST requests for JSON
 import bodyParser from 'body-parser';
 
 
 //Connect to database, replace with your own .env file
 var test_database = await mysql.createConnection({
-    host: "db",    //Set name of network host
-    user: "root",         //Set username, in this case root
-    password: "default_pass", //Set password
-    database: "placeholder"
+    host: "db",                 //Set name of network host
+    user: "root",               //Set username, in this case root
+    password: "default_pass",   //Set password
+    database: "placeholder"     //Set Database Name
 }) 
 
 
@@ -20,45 +25,61 @@ test_database.connect(function(err) {
     console.log("Connected!");
   });
 
+//Queries database for the entire table specified in createConnection()
+async function queryDatabase() {
+    //Results will store and return the query, default to -1 for testing
+    var results = -1;
 
-var result = "default value"; 
+    try{     
+        //Get all rows from table
+        results = await test_database.query(
+            'SELECT * FROM `test_table`');   
+    }
+    //If there's an error, log it to console. 
+    catch (err) {
+        console.log(err);
+    }
 
-//Query the database
-try{
-    const [results] = await test_database.query(
-        'SELECT * FROM `test_table`'
-    );
-
-    result = results;
-
-} catch (err) {
-    console.log(err);
+    return results; 
 }
-
 
 //Now start the server
 const app = express();
+//Use cors for cross site access
 app.use(cors({}));
-
 //Use JSON body parser middleware
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 
 //If you send an http post request. 
 app.post("/api/posts", (req, res) => {
     //Print body
     console.log(req.body);
+
+    //Clean Data
+
+    //Send data to MySQL server
+
+    //Refresh page
     res.redirect("http://localhost:3000");
 });
 
-//If you send an http get request.
+//If you send an http get request...
 app.get("/", (req, res) => {
-    res.send(result);
-})
 
+    //Clean the response (if needed)
+
+    //Send the table. 
+    queryDatabase()
+     .then((data) => res.send(data[0]));
+});
+
+
+//Assign port
 const PORT = process.env.PORT || 8080;
 
+//Start listening on port
 app.listen(PORT,
     console.log(`Server started on port ${PORT}`)
 );
